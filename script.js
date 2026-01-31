f505b63c3f9f78eb986a655a2987d8cecfa7d80d
Kaushik Mer <69963864+Kaushik-Mer@users.noreply.github.com>
2026-01-31 09:01:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -103,7 +103,3 @@ document.addEventListener('touchstart', function(e) {
         e.preventDefault();
     }
 }, { passive: false });
-
-document.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-}, { passive: false });
